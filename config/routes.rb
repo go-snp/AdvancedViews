912eab5d54a999404a96e945cef0782cd6f9960d
go-snp <ogawa.gosuke@gmail.com>
2017-03-14 20:59:34 +0900

    BlogとEntryがネストされていなかったので修正。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :blogs, only: %i(index new create show edit update destroy)
-  resources :entries, only: %i(new create show edit update destroy)
+  resources :blogs, only: %i(index new create show edit update destroy) do
+    resources :entries, only: %i(new create show edit update destroy)
+  end
+  
 end
