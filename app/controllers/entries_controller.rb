239d3d0dca43fa7d6e0622563b03567fbaf09453
go-snp <ogawa.gosuke@gmail.com>
2017-03-17 21:54:28 +0900

    EntriesControllerにて、親子関係を無視してEntryをShowできないよう修正。
    同時に、EntriesControllerのset_XXXメソッドをset_entry_and_blogに統一（混乱防止のため）

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,32 @@
 class EntriesController < ApplicationController
-  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :set_entry_and_blog, only: [:show, :new, :create, :edit, :update, :destroy]
 
   def show
-    @blog = @entry.blog
+  end
+
+  def new
+    @entry = @blog.entries.new
+  end
+
+  def create
+    @entry = @blog.entries.new(entry_params)
+
+    if @entry.save()
+      redirect_to blog_entry_path(@blog, @entry), notice: "Entry was successfully created."
+    else
+      render :new
+    end
+
   end
 
   private
 
-    def set_entry
-      @entry = Entry.find(params[:id])
+    def set_entry_and_blog
+      @entry = Entry.find_by(id: params[:id], blog_id: params[:blog_id])
+      @blog = Blog.find(params[:blog_id])
     end
 
     def entry_params
-      params.require(:entry).permit(:title, :body, :status)
+      params.require(:entry).permit(:title, :body)
     end
 end
